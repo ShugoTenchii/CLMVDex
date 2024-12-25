8ef92b8608ce80d9d0ebba0ab08e2f4c35cf5bc8
ShugoTenchii <MLKwi6057@gmail.com>
2024-12-25 13:03:16 +0100

    Initial commit

---
+++ CLMVDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  CLMVDexApp.swift
+//  CLMVDex
+//
+//  Created by ShugoTenchi on 25/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CLMVDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
